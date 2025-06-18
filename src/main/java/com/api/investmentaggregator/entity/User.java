f2fa61f0d5f6611f3f25372afb2234e57261b1e0
Guilherme <amaroguilherme99@gmail.com>
2025-06-18 15:42:42 -0300

    feat. creating the entity user.

---
+++ src/main/java/com/api/investmentaggregator/entity/User.java
@@ -0,0 +1,34 @@
+package com.api.investmentaggregator.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "tb_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "name")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @CreationTimestamp
+    private Instant creationTimestamp;
+
+    @UpdateTimestamp
+    private Instant updateTimestamp;
+}
